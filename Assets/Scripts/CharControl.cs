d195d4ac4f644d48271badd119f73f6abad6293b
Justin Ha <jch583@nyu.edu>
2016-10-04 16:18:18 -0400

    Deducted code to only what is necessary and commented out potential future use code

--- Assets/Scripts/CharControl.cs
+++ Assets/Scripts/CharControl.cs
@@ -17,26 +17,13 @@ public class CharControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-//		float inputX = Input.GetAxis ("Horizontal"); // A/D keys control movement 
-//		float inputY = Input.GetAxis ("Vertical"); // W/S keys control movement 
 
 		float MouseX = Input.GetAxis ("Mouse X"); // Turn left or right with mouse 
-		float MouseY = Input.GetAxis ("Mouse Y"); // Look up or down 
+		//float MouseY = Input.GetAxis ("Mouse Y"); // Look up or down 
 
-		//Moves the player 
-		//cController.SimpleMove(transform.forward * inputY * moveSpeed + transform.right * inputX * moveSpeed); 
-
-		//Player controls the view 
-		transform.Rotate(-MouseY * cameraSpeed * Time.deltaTime, MouseX * cameraSpeed * Time.deltaTime, 0f); 
-		//transform.Rotate (-MouseY * 5f * Time.deltaTime, 0f, 0f); 
-		transform.eulerAngles = new Vector3 (transform.eulerAngles.x, transform.eulerAngles.y, 0f);
+		//Player can use the mouse to turn left or right 
+		transform.Rotate(0f, MouseX * cameraSpeed * Time.deltaTime, 0f); 
 
 
 	}
-
-//	void OnTriggerEnter (Collider coll){
-//		if (coll.gameObject.tag == "Bed" && (Input.GetKeyDown(KeyCode.Space))){
-//			playerPosition = bedPostion;
-//		}
-//	}
 }
