e5169cfb1fdce7feffac2ed5d10e04de6d5ea33e
Justin Ha <jch583@nyu.edu>
2016-10-04 16:13:37 -0400

    New game idea Trouble Sleeping Game Made player start on the bed and can only look around

--- Assets/Scripts/CharControl.cs
+++ Assets/Scripts/CharControl.cs
@@ -3,27 +3,28 @@ using System.Collections;
 
 public class CharControl : MonoBehaviour {
 
-	CharacterController cController; 
+	//CharacterController cController; 
 
 	public float moveSpeed; 
 	public float cameraSpeed; 
 
 	// Use this for initialization
 	void Start () {
-		cController = GetComponent<CharacterController> (); 
+		//cController = GetComponent<CharacterController> (); 
+		//Vector3 playerPosition = transform.position; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		float inputX = Input.GetAxis ("Horizontal"); // A/D keys control movement 
-		float inputY = Input.GetAxis ("Vertical"); // W/S keys control movement 
+//		float inputX = Input.GetAxis ("Horizontal"); // A/D keys control movement 
+//		float inputY = Input.GetAxis ("Vertical"); // W/S keys control movement 
 
 		float MouseX = Input.GetAxis ("Mouse X"); // Turn left or right with mouse 
 		float MouseY = Input.GetAxis ("Mouse Y"); // Look up or down 
 
 		//Moves the player 
-		cController.SimpleMove(transform.forward * inputY * moveSpeed + transform.right * inputX * moveSpeed); 
+		//cController.SimpleMove(transform.forward * inputY * moveSpeed + transform.right * inputX * moveSpeed); 
 
 		//Player controls the view 
 		transform.Rotate(-MouseY * cameraSpeed * Time.deltaTime, MouseX * cameraSpeed * Time.deltaTime, 0f); 
@@ -32,4 +33,10 @@ public class CharControl : MonoBehaviour {
 
 
 	}
+
+//	void OnTriggerEnter (Collider coll){
+//		if (coll.gameObject.tag == "Bed" && (Input.GetKeyDown(KeyCode.Space))){
+//			playerPosition = bedPostion;
+//		}
+//	}
 }
