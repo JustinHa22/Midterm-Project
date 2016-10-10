fd73780140c09cee95478983a1f528c102e345e7
Justin Ha <jch583@nyu.edu>
2016-10-10 16:52:36 -0400

    All Sheep scripts have been applied to sheep

--- Assets/Scripts/Sheep2.cs
+++ Assets/Scripts/Sheep2.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class Sheep2 : MonoBehaviour {
 
+	public float sheepSpeed; 
+	public Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		rb.AddForce(new Vector2(-sheepSpeed * Time.deltaTime, 0f), ForceMode.Impulse);
 	}
 }
