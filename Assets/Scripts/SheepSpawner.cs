f86ad32fe04abd049d8ea135703059f798aaee7e
Justin Ha <jch583@nyu.edu>
2016-10-10 17:26:07 -0400

    Got spawner to work

--- Assets/Scripts/SheepSpawner.cs
+++ Assets/Scripts/SheepSpawner.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class SheepSpawner : MonoBehaviour {
 
 	public GameObject[] Sheep;
+	int bufferTime = 0; 
+	int restartBuffer = 100; 
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +14,13 @@ public class SheepSpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		int randomSheep = Random.Range (0, Sheep.Length);
+
+		bufferTime += 1;
+
+		if (bufferTime == restartBuffer) {
+			Instantiate (Sheep [randomSheep], transform.position, Quaternion.identity);
+			bufferTime = 0;
+		}
 	}
 }
