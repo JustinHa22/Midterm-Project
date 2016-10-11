90def6fedc5c559fa2af38d42bd276b754fa023f
Justin Ha <jch583@nyu.edu>
2016-10-10 21:34:54 -0400

    Got spring to work

--- Assets/Scripts/Sheep3.cs
+++ Assets/Scripts/Sheep3.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Sheep3 : MonoBehaviour {
 
 	public float sheepSpeed; 
+	public float Spring;
 	public Rigidbody rb;
 
 	// Use this for initialization
@@ -14,5 +15,11 @@ public class Sheep3 : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		rb.AddForce(new Vector2(-sheepSpeed * Time.deltaTime, 0f), ForceMode.Impulse);
+	}
+		
+	void OnTriggerStay(Collider col){
+		if (col.gameObject.tag == "Spring" && Input.GetKeyDown (KeyCode.Space)) {
+			rb.AddForce (new Vector2 (0f, Spring * Time.deltaTime), ForceMode.Impulse);
+		}
 	}
 }
