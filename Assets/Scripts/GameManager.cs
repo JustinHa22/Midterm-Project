f4c58474f7ce3b506318f01bd1038c1fd7377643
Justin Ha <jch583@nyu.edu>
2016-10-18 02:38:01 -0400

    Fading working more efficiently with new code, but not working on Dream Scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,21 +8,41 @@ public class GameManager : MonoBehaviour {
 	public float fadeOutSpeed; // The fading speed 
 
 	private bool fallingAsleep = false;
-	static bool enteringDream; 
+	private bool sleepTime = false; 
+
+	private bool enteringDream = false;
+	//private bool wakeUp = false; 
 
 	private int drawDepth = -1000; // The texture's order in the draw hierarchy: a low number means it renders on top 
-	private float alpha = 0f; // The texture's alpha value between 0 and 1 
+	public float alpha; // The texture's alpha value between 0 and 1 
 	public int fadeDirection; // The direction to fade: -1 = fade in and 1 = fade out
 
 	private int fadeBuffer = 0; 
 
-	static int sceneChange = 0; 
+
+	void Start(){
+		Scene currentScene = SceneManager.GetActiveScene ();
+
+		string sceneName = currentScene.name;
+
+		if (sceneName == "Sleep Scene") {
+			sleepTime = true; 
+			Debug.Log ("works");
+		}
+
+		if (sceneName == "Dream Scene") {
+			enteringDream = true;
+		}
+
+	}
 
 	void OnGUI (){
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			fallingAsleep = true;
+		if (sleepTime == true) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				fallingAsleep = true;
+			}
 		}
-		if (fallingAsleep ) {
+		if (fallingAsleep == true) {
 			// Fade out/in the alpha value using a direction, a speed and Time.deltaTimeto convert the operation to seconds
 			alpha += fadeDirection * fadeOutSpeed * Time.deltaTime; 
 			// Force (clamp) the number between 0 and 1 because GUI.color uses alpha values between 0 and 1
@@ -31,31 +51,32 @@ public class GameManager : MonoBehaviour {
 			fadeBuffer += 1; 
 			if (fadeBuffer == 250) {
 				enteringDream = true;
-				sceneChange += 1; 
 				fadeBuffer = 0; 
 				SceneManager.LoadScene (1);
 
-//				sceneChange += 1; 
-//				fadeBuffer = 0; 
+
 			}
 
 		}
+			
 
-		if (sceneChange % 2 == 1) {
-			if (enteringDream) {
-				alpha = 1;
-				enteringDream = false;
-			}
-
-			Debug.Log (alpha + " " + fadeDirection);
-
+		if (enteringDream == true) {
 			// Fade out/in the alpha value using a direction, a speed and Time.deltaTimeto convert the operation to seconds
 			alpha += fadeDirection * fadeOutSpeed * Time.deltaTime; 
 			// Force (clamp) the number between 0 and 1 because GUI.color uses alpha values between 0 and 1
 			alpha = Mathf.Clamp01 (alpha);
 
 			fadeBuffer += 1; 
+			enteringDream = false;
 		}
+			
+
+//			// Fade out/in the alpha value using a direction, a speed and Time.deltaTimeto convert the operation to seconds
+//			alpha += fadeDirection * fadeOutSpeed * Time.deltaTime; 
+//			// Force (clamp) the number between 0 and 1 because GUI.color uses alpha values between 0 and 1
+//			alpha = Mathf.Clamp01 (alpha);
+//
+//			fadeBuffer += 1; 
 
 			// Set color of our GUI (in this case our texture). All color values remain the same & the Alpha is set to the alpha variable
 			GUI.color = new Color (GUI.color.r, GUI.color.g, GUI.color.b, alpha);	//set the alpha value
